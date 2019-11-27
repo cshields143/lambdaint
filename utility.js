7829d1e6490693e064b1b0675926f3df43eb3e46
chris <christopher.shields143@gmail.com>
2019-11-27 11:41:34 -0800

    getting the pairs of parentheses

---
+++ utility.js
@@ -0,0 +1,25 @@
+const ParError = class extends Error {
+  constructor(kind, locs, orig) {
+    super(kind);
+    Error.captureStackTrace(this, ParError);
+    this.locs = locs;
+    this.str = orig;
+  }
+}
+
+const get_par_pairs = cps => {
+  const bucket = [];
+  const pairs = [];
+  cps.forEach((cp, i) => {
+    if (cp === '(') bucket.push(i);
+    else if (cp === ')') {
+      if (!bucket.length) throw new ParError('closing', [i], cps.join(''));
+      else {
+        const open = bucket.pop();
+        pairs.push([open, i]);
+      }
+    }
+  });
+  if (bucket.length) throw new ParError('opening', bucket, cps.join(''));
+  return pairs;
+};
